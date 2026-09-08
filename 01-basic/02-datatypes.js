28a4e9e2b80ac629482d0196c166e8805516a168
umaralam123 <81714744+umaralam123@users.noreply.github.com>
2026-09-08 07:10:43 +0000

    new file

---
+++ 01-basic/02-datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code in new version
+// alter (3+3)  we are using nodejs not a browser //
+
+console.log('umar'); // code readability should be high
+
+let name="Umar Alam"
+let age =29
+let isLoggedIn= false 
+let state;
+
+/*
+Data types
+number => 2 to 53
+string => "abc"
+bigint =>
+boolen => true, false
+null => standalone value 
+undefined =>
+symbol=> unique
+object
+
+you check the null type output show the object 
+*/
+
+console.log(typeof age)
