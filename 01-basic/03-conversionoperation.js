475566e8b117c046baa19fba8633f348a1be1de0
umaralam123 <81714744+umaralam123@users.noreply.github.com>
2026-09-08 08:03:06 +0000

    third file

---
+++ 01-basic/03-conversionoperation.js
@@ -0,0 +1,34 @@
+let score= "33abc"
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+Conversion
+
+33=> 33
+"33abc"=> NAN
+TRUE=1 FALSE=0
+
+*/
+
+let isLoggedIn =1
+let boolenisLoggedn =Boolean(isLoggedIn)
+console.log(boolenisLoggedn);
+
+/*
+1=> true  false => 0
+"" =>false
+"umar"  => true
+*/
+
+let someNumber =33
+let stringNumber = string(someNumber)
+
+console.log(someNumber);
+console.log(typeof stringNumber);
+
