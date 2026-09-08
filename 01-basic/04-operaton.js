d863e34cadff2b0192c4d8eea7116578fe593e61
umaralam123 <81714744+umaralam123@users.noreply.github.com>
2026-09-08 08:38:04 +0000

    4file

---
+++ 01-basic/04-operaton.js
@@ -0,0 +1,44 @@
+let value= 3
+let negValue =-value
+console.log(negValue);
+
+/*
+console.log(2+2); add
+console.log(2-2);  subtract
+
+console.log(2*2);  multoplication
+console.log(2**2); power
+
+console.log(2/2); divided
+console.log(2%2); reminder  5%2  1 is reminder
+*/
+
+let str1 = 1
+let str2 = 4
+
+let str3 = str1 +str2 
+
+console.log(str3);
+
+// console.log(1 +'2');
+// console.log('1' + 2);
+// console.log('1'+2+2);
+// console.log(1+2+'2');
+
+// console.log((3+4)*3 / 3);  
+
+let num1 , num2, num3;
+num1= num2 = num3= 3*3 ;
+
+// anser is 9 teeno ki same value hn 9
+
+let gameCounter = 100
+++gameCounter ;  //prefix
+console.log(gameCounter);
+gameCounter++    //post fix
+
+console.log(gameCounter);
+
+
+
+
