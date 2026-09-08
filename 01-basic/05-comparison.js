564bba59f21f89c31d9c69f0c83294bc33a5fdce
umaralam123 <81714744+umaralam123@users.noreply.github.com>
2026-09-08 08:56:45 +0000

    05 file

---
+++ 01-basic/05-comparison.js
@@ -0,0 +1,20 @@
+// console.log(2==1);
+// console.log(2!= 1);
+
+// console.log("2" >1);
+// console.log("02" >1);  //comparsion not give predictible 
+
+// console.log(null >0);
+// console.log(null== 0);
+// console.log(null >= 0);
+// aus tara ka comparison ko aviod karna hn clean code karna hn  ausa periority dani han 
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log (undefined = 0)
+//====
+
+console.log("2" === 2);
+
+
+
+
