470011ab0592f6639c7e34860c2eaf3f71f9142f
umaralam123 <81714744+umaralam123@users.noreply.github.com>
2026-09-10 08:27:56 +0000

    09 time

---
+++ 01-basic/09-date.js
@@ -0,0 +1,25 @@
+let myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let createMyDate = new Date (1995,8, 29)
+
+console.log(createMyDate.toDateString());
+
+let myTimeStamp = Date. now()
+// console.log(myTimeStamp);
+// console.log(createMyDate.getTime());
+// console.log(Date.now());
+console.log(Math.floor(Date.now()/1000));
+
+
+
+let newDate = new Date ()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
